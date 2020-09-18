7a7d76f68f2c102434f784a2b6b8554359234dfb
uc-kunkelad <kunkelad@ucmail.uc.edu>
2020-09-17 22:01:43 -0400

    Adding powershell files

---
+++ powershell/sysinfo.ps1
@@ -0,0 +1,47 @@
+﻿function getIP{
+    (Get-NetIPAddress -InterfaceAlias Ethernet0).ipv4address
+}
+
+$IP = getIP
+#write-host("This machine's IP is $IP.")
+#write-host("This machine's IP is {0}." -f $IP)
+
+#$Body = "This machine's IP is $IP."
+
+#write-host($Body)
+
+function getHostname{Hostname}
+
+$getHostname = getHostname
+
+write-host $getHostname
+
+
+
+function getUser{whoami}
+
+$getUser = getUser
+
+write-host $getUser
+
+
+function getVersion{$HOST.version.Major}
+
+$getVersion = getVersion
+
+write-host $getVersion
+
+
+
+function getDate{DATE}
+
+$getDate = DATE
+
+write-host $getDate
+
+$Body = "This Email was sent on behalf of Adam Kunkel for IT3038C. The machine's IP address is $IP. The machine's name is $getHostname. The current user is $getUser. The current Powershell version is $getVersion. The current date is $getDate."
+
+write-host $Body
+
+Send-MailMessage -To "botheaj@ucmail.uc.edu" -From "adam.j.kunkel@gmail.com" -Subject "IT3038C Windows Sysinfo" -Body $Body -SmtpServer "smtp.gmail.com" -port 587 -UseSsl -Credential (Get-Credential)
+
