12e35cab7383d9d1d349283e10085bbaa9921541
uc-kunkelad <kunkelad@ucmail.uc.edu>
2020-09-24 19:44:26 -0400

    changes made to script comments

--- bash/gitbackup.sh
+++ bash/gitbackup.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
-#This script will create a compressed backup of a local git directory in your homedir
-#replace it3038c-scripts with the name of the git directory in your homedir
+#This script will create a compressed backup of a local git directory located in your homedir
+#~/it3038c-scripts is the directory that will be backed up
+#replace it3038c-scripts with the name of your git directory in your homedir
 #tar is the command for a Linux backup
 #c for create, v for verbose (not necessary for automated functions) p to preserver permissions, z to compress, and f to add a filename
-#/gitbackup is the filename and location. In this case it is in the root dir
-#~/it3038c-scripts is the directory that will be backed up
+#/gitbackup.tar.gz is the filename and location of the backup. In this case it is in the root dir
 #.tar denotes a backup while .tar.gz denotes a compressed backup
+
+#make sure gitbackup.sh is executable with chmod +x gitbackup.sh
+#run with ./gitbackup.sh
 
 sudo tar -cvpzf /gitbackup.tar.gz ~/it3038c-scripts
