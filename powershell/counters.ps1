5013816448dac6a85a0c7e32d17fa8ced54234fa
uc-kunkelad <kunkelad@ucmail.uc.edu>
2020-10-17 20:49:03 -0400

    added modules

--- powershell/counters.ps1
+++ powershell/counters.ps1
@@ -5,6 +5,11 @@
 #}
 
 Foreach ($machine in $Machines) {
-    $pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time").CounterSamples.CookedValue
-    $pt
+    $pt = (Get-Counter -Counter "\Processor(_Total)\% Processor Time" -SampleInterval 1 -MaxSamples 10).CounterSamples.CookedValue
+        $sample = 1
+        foreach ($p in $pt) {
+            $date = Get-Date -Format g
+            "Sample {3}: CPU is at {0}% on {1}" -f [int]$p, $machine, $sample, $date | out-file -append C:\logs\output.txt
+        $sample++
+    }
 }
---
+++ powershell/tools.psm1
@@ -0,0 +1,7 @@
+﻿function getIP{
+    (Get-NetIPAddress -InterfaceAlias Ethernet0).ipv4address
+}
+
+function sayHello {
+    write-host("Hello, user!")
+}
