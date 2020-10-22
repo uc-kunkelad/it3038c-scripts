dac8d1ce5ddc399e4bcfaaeea7334a74c7ce7ca6
uc-kunkelad <70353756+uc-kunkelad@users.noreply.github.com>
2020-10-22 19:11:12 -0400

    Update project2.sh
    changed variable capitalization

--- project2/project2.sh
+++ project2/project2.sh
@@ -7,15 +7,15 @@ alertUse=75%
 #retrieves server hostname
 server=$HOSTNAME
 #enter your email below
-emailaddress=kunkelad@ucmail.uc.edu
+emailAddress=kunkelad@ucmail.uc.edu
 
 #if disk use is below 75%, an email is sent without warning
 if [ ${diskUse%?} -lt ${alertUse%?} ]
 then
         mailBody="Disk space on $server is OK. Your disk is at ${diskUse} capacity."
-        mail -s "Disk Space OK" $emailaddress <<< $(echo -e $mailBody)
+        mail -s "Disk Space OK" $emailAddress <<< $(echo -e $mailBody)
 #if disk use is 75% or above, an email is sent with a warning
 else
         mailBody="Disk space on $server is near max. Your disk is at ${diskUse} capacity."
-        mail -s "Disk Space Alert!" $emailaddress <<< $(echo -e $mailBody)
+        mail -s "Disk Space Alert!" $emailAddress <<< $(echo -e $mailBody)
 fi
