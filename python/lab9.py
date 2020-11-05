6e3c208ec64381bf38a3257a65bdf15ad2e4369c
uc-kunkelad <kunkelad@ucmail.uc.edu>
2020-11-05 18:25:25 -0500

    added for loop to lab9.py

--- python/lab9.py
+++ python/lab9.py
@@ -5,12 +5,5 @@ r = requests.get('http://localhost:3000')
 
 data = r.json()
 
-print(data)
-print(data[0]['name'] + " is...")
-print(data[0]['color'] + "!")
-print(data[1]['name'] + " is...")
-print(data[1]['color'] + "!")
-print(data[2]['name'] + " is...")
-print(data[2]['color'] + "!")
-print(data[3]['name'] + " is...")
-print(data[3]['color'] + "!")
+for d in data:
+    print(d['name'] + " is " + d['color'])
