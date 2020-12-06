920a027f25a4e805f48b8a72bbaab476742ab95a
uc-kunkelad <kunkelad@ucmail.uc.edu>
2020-12-06 11:30:56 -0500

    adding project 3

---
+++ project3/project3.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#calculates disk free space
+diskUse=$(df -h | grep '/dev/mapper/centos-root' | awk {'print $5'})
+#variable to set disk usage alert percentage
+alertUse=75%
+#retrieves server hostname
+server=$HOSTNAME
+#enter your email below
+emailAddress=kunkelad@ucmail.uc.edu
+
+#if disk use is below 75%, an email is sent without warning
+if [ ${diskUse%?} -lt ${alertUse%?} ]
+then
+        mailBody="Disk space on $server is OK. Your disk is at ${diskUse} capacity."
+        mail -s "Disk Space OK" $emailAddress <<< $(echo -e $mailBody)
+#if disk use is 75% or above, an email is sent with a warning
+else
+        mailBody="Disk space on $server is near max. Your disk is at ${diskUse} capacity."
+        mail -s "Disk Space Alert!" $emailAddress <<< $(echo -e $mailBody)
+fi
